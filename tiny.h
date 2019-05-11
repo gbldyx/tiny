0059bba337432e1f77ea441d7d37874c36a66772
gbldyx <43329094+gbldyx@users.noreply.github.com>
2019-05-11 15:13:20 +0800

    Add files via upload

---
+++ tiny.h
@@ -0,0 +1,28 @@
+typedef enum { typeCon, typeId, typeOpr} nodeEnum;
+
+typedef struct{
+	nodeEnum type;
+	int value;
+}conNodeType;
+
+typedef struct{
+	nodeEnum type;
+	int i;
+}idNodeType;
+
+typedef struct{
+	nodeEnum type;
+	int oper;
+	int nops;
+	union nodeTypeTag *op[1];
+}oprNodeType;
+
+typedef union nodeTypeTag{
+	nodeEnum type;
+	conNodeType con;
+	idNodeType id;
+	oprNodeType opr;
+}nodeType;
+
+extern int sym[26];
+
